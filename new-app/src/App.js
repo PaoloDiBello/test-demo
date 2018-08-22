974568358f8a45c784be0ff0ef7545ecc44d2d84
Paolo Di Bello <dibellopol@gmail.com>
2018-08-22 19:36:37 +0200

    updated

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Products from './Products';
+
 class App extends Component {
     state = {
         products: [
-            { wineName: 'Red', source: 'NY', winePrice: 30, quantity_bottle: 4, id: 1 },
-            { wineName: 'White', source: 'LA', winePrice: 40, quantity_bottle: 5, id: 2 },
-            { wineName: 'Pink', source: 'CA', winePrice: 50, quantity_bottle: 6, id: 3 },
+            { wineName: 'Red', img: 'http://www.viniexport.com/trading/storage/2017/January/week4/60273_RUBRATO-AGLIANICO.png', source: 'NY', winePrice: 30, quantity_bottle: 4, id: 1 },
+            { wineName: 'White', img: 'http://www.viniexport.com/trading/storage/2018/January/week1/80263_CAVIT-ALTEMASI-MILLESIMATO.png', source: 'LA', winePrice: 40, quantity_bottle: 5, id: 2 },
+            { wineName: 'Pink', img: 'http://www.viniexport.com/trading/storage/2017/February/week2/63622_L_408.png', source: 'CA', winePrice: 50, quantity_bottle: 6, id: 3 },
         ]
+
     }
 
     render() {
         return (
             <div className="App">
-                <h1>My first React app</h1>
+                <h1>My first test using React</h1>
                 <Products products={this.state.products} />
             </div>
         );
--- new-app/src/Products.js
+++ new-app/src/Products.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 
 class Products extends Component {
     state = {
         quantity: 1,
-        key: ''
+        key: 0,
+        show: false,
     }
 
-    handleChange(event) {
-        this.setState({ quantity: event.target.value })
+    handleClose() {
+        this.setState({ show: false });
+    }
 
+    handleShow() {
+        this.setState({ show: true });
+    }
+
+
+    handleChange(event) {
+        this.setState({ quantity: event.target.value, key: event.target.id })
     }
 
 
@@ -19,16 +29,48 @@ class Products extends Component {
 
                 <div className="wineBox" key={product.id} data-id={product.id}>
 
-                    Select quantity :
-                    <div>Quantity: <input type="number" name="quantity_bottle" className="rsQuantity wineQuantity" step={1} max={product.quantity_bottle} defaultValue={1} min={1} onChange={this.handleChange.bind(this)} />
-                        &nbsp;<img src={"http://www.viniexport.com/trading/vximg/wineBottleIcon.png"} className="wineBottleIcon" alt="aa" /></div>
-                    <div className="wineName">Name: {product.wineName}<br /><small>{product.source}</small></div>
-                    <input type="hidden" className="rsPrice" name="prezzo_vendita" value={product.winePrice} />
-                    <input type="hidden" name="etichetta_id" value={product.id} />
-                    <div>Subtotal: {product.quantity_bottle}</div>
-                    <div>Bottles: {product.winePrice * this.state.quantity}</div>
+
+                    <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Modal heading</Modal.Title>
+                            <img className="wineLabelBoxBg img-fluid" src={product.img} alt={product.wineName} />
+                        </Modal.Header>
+                        <Modal.Footer>
+                            <Button onClick={this.handleClose.bind(this)}>Close</Button>
+                        </Modal.Footer>
+
+                    </Modal>
+                    <div className="wineBoxColLx">
+                        <div className="zoomWineLabel">
+                            <div className="pull-right"><i className="fa fa-plus-square pull-right" data-toggle="modal" data-target="#zoomImageModal" onClick={this.handleShow.bind(this)}></i>
+                            </div>
+                            <div className="wineLabelBox">
+                                <img className="wineLabelBoxBg img-fluid" src={product.img} alt={product.wineName} />
+                            </div>
+
+                        </div>
+                        <a onClick={this.handleShow.bind(this)}>Product Sheet</a>
+                    </div>
+                    <div class="wineBoxColRx">
+
+                        Select quantity :
+                    <div>Quantity: <input type="number" name="quantity_bottle" className="rsQuantity wineQuantity" step={1} max={product.quantity_bottle} defaultValue={1} min={1} onChange={this.handleChange.bind(this)} id={product.id} />
+                            &nbsp;<img src={"http://www.viniexport.com/trading/vximg/wineBottleIcon.png"} className="wineBottleIcon" alt="bottle_icon" /></div>
+                        <div className="wineName">Name: {product.wineName}<br /><small>{product.source}</small></div>
+                        <input type="hidden" className="rsPrice" name="prezzo_vendita" value={product.winePrice} />
+                        <input type="hidden" name="etichetta_id" value={product.id} />
+                        <div>Subtotal: {product.quantity_bottle}</div>
+                        <div>Bottles: {(this.state.key + "") === (product.id + "") ? product.winePrice * this.state.quantity : product.winePrice}</div>
+
+                    </div>
+                    <div className="clearfix" />
+
+                    <div className="winemakerName" title={product.wineName}>{product.wineName}</div>
+
 
                 </div>
+
+
 
             )
         });
